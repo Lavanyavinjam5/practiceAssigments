bbddbbb3e6ae71709b1ee06f90c7db230c62eb84
Lavanyavinjam <lavanyavinjam5@gmail.com>
2025-01-18 09:20:38 +0530

    Smartcontrollerapp

---
+++ app/src/main/java/com/example/smarthomecontroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.smarthomecontroller
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.smarthomecontroller.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        //  binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main) //R.layout.activity_main
+
+        //  setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        //initialising the Navigation controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Setup ActionBar with NavController
+        setupActionBarWithNavController(navController)
+
+
+    }
+}
